2eb00ec8f68cef1ef25a325e06aa138bb4ae64a3
Maksim Yurevich <exile-path-mud@duck.com>
2025-07-03 02:24:51 +0300

    feat: update `config.fish`
    * Change `BROWSER` variable to `brave-browser`
    * Remove `LIBVA_DRIVER_NAME` variable
    * Remove `pyenv` config
    * Add `rust` source
    * Add `kubectl` completion
    * Add `storePathForWindowsTerminal` function
    * Add Tokyonight Storm theme

--- config.fish
+++ config.fish
@@ -9,11 +9,7 @@ set -x VISUAL nvim
 set -x EDITOR nvim
 set -x PAGER "less -R"
 set -x BAT_THEME ansi
-set -x BROWSER /usr/bin/chromium
-
-# wsl
-
-set -Ux LIBVA_DRIVER_NAME d3d12
+set -x BROWSER /usr/bin/brave-browser
 
 fish_add_path $HOME/.local/share/nvim/mason/bin
 fish_add_path $HOME/.local/bin
@@ -23,7 +19,7 @@ fish_add_path $HOME/.npm/bin
 
 # fzf
 
-set -gx FZF_DEFAULT_OPTS "
+set -Ux FZF_DEFAULT_OPTS "
 --color=fg:#c0caf5,bg:#24283b,hl:#ff9e64
 --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64
 --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
@@ -33,13 +29,6 @@ set -gx FZF_DEFAULT_OPTS "
 
 set -ga fish_user_paths "$HOME/.rye/shims"
 rye self completion -s fish >~/.config/fish/completions/rye.fish
-
-# pyenv
-
-set -gx PYENV_ROOT $HOME/.pyenv
-set -g fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
-pyenv init - fish | source
 
 # mise
 
@@ -56,7 +45,27 @@ zoxide init fish | source
 # ssh
 
 if not pgrep --full ssh-agent | string collect >/dev/null
-  eval (ssh-agent -c)
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
+
+# rust
+
+source "$HOME/.cargo/env.fish"
+
+# kubectl
+
+kubectl completion fish | source
+
+# wsl
+
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+        printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
+
+# theme
+
+theme_tokyonight storm
